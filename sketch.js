c70d2cc423d7492c006d7f2de2589a76085c1141
maxim00000maxim <63451409+maxim00000maxim@users.noreply.github.com>
2020-05-01 16:15:28 +0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+let r = 0
+
+function setup() {
+  createCanvas(600,600);}
+
+function draw() {
+  background(100);
+ 
+ rect(1,1,random(0,400),random(0,400))
+  rect(1,1,random(0,400),random(0,400))
+   rect(1,1,random(0,400),random(0,400))
+   rect(1,1,random(0,400),random(0,400))
+   rect(1,1,random(0,400),random(0,400))
+  
+  scale(-1,-1);
+    rect(1,1,random(0,400),random(0,400))
+  rect(1,1,random(0,400),random(0,400))
+   rect(1,1,random(0,400),random(0,400))
+   rect(1,1,random(0,400),random(0,400))
+   rect(1,1,random(0,400),random(0,400))
+  
+}
